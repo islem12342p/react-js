f53cb86b0f67a2eaf14b6f5597f673b3a88216c0
islem12342p <144261545+islem12342p@users.noreply.github.com>
2023-12-06 14:13:34 +0100

    Add files via upload

---
+++ react_js/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Cards from "./components/Cards";
+import Header from "./components/Header";
+import Logins from "./components/Logins";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      
+      <Header />
+      
+      <Cards />
+      <Logins />
+      
+
+    </div>
+  );
+}
+
+export default App;
